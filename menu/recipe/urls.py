5c1cfa451b45d873e958ab6fed27733df09fa935
pranavv46 <vinupoojavinukumar@gmail.com>
2026-06-15 14:11:35 +0530

    added specif url

--- menu/recipe/urls.py
+++ menu/recipe/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('recipe/', views.recipe_list, name='recipe_list'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
 ]
--- menu/recipe/views.py
+++ menu/recipe/views.py
@@ -5,3 +5,16 @@ def home(request):
     return render(request, 'home.html', {
         'name': 'Pranav'
     })
+
+from .models import Recipe
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes.html', {
+        'recipes': recipes
+    })
+
+def recipe_detail(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    return render(request, 'recipe_detail.html', {
+        'recipe': recipe
+    }) 
